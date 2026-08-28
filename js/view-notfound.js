7923fe40d9e60c269c1b200a00b6b406d5bdf3a4
redrighthand2007 <redrighthand2007@gmail.com>
2026-08-28 09:44:12 +0530

    cleaned up code

--- js/view-notfound.js
+++ js/view-notfound.js
@@ -7,11 +7,6 @@ window.ViewNotfound = {
             <h2 style="font-size: 2rem; margin-bottom: 20px;">Out of Coverage Area</h2>
             <p style="color: var(--text-muted); font-size: 1.2rem; max-width: 500px; margin: 0 auto 40px;">
                 Looks like the page you are looking for isn't covered by our policy. Let's get you back to safety.
-                <br><br>
-                <small style="color: red;">DEBUG LOG:<br>
-                Hash: \${window.location.hash}<br>
-                ViewDashboard type: \${typeof window.ViewDashboard}
-                </small>
             </p>
             <a href="#home" class="btn btn-primary pulse-btn">Return Home</a>
         </div>
